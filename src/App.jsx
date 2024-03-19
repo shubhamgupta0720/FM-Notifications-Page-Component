9224332e37999eb3a64336d7c06b501006f17404
Shubham <shubhamgupta0720@gmail.com>
2024-03-19 22:03:46 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <div id="notificationCard">
+          <div className="title-mark">
+            <div className="title-notif-count">
+              <h1>Notifications</h1>
+              <label>3</label>
+            </div>
+            <a>Mark all as read</a>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
