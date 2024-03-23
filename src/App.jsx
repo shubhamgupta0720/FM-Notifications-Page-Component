ac499f6304756cbb9cefdc53e30791b9b1ea00f9
Shubham <shubhamgupta0720@gmail.com>
2024-03-23 10:17:30 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import Notification from './components/Notification'
 import markWebberProfile from "../src/assets/images/avatar-mark-webber.webp"
 import angelaGrey from "../src/assets/images/avatar-angela-gray.webp"
 import jacobThompson from "../src/assets/images/avatar-jacob-thompson.webp"
+import rizkyHasanuddin from "../src/assets/images/avatar-rizky-hasanuddin.webp"
+import kimberlySmith from "../src/assets/images/avatar-kimberly-smith.webp"
+import nathanPeterson from "../src/assets/images/avatar-nathan-peterson.webp"
+import annaKim from "../src/assets/images/avatar-anna-kim.webp"
+import imageChess from "../src/assets/images/image-chess.webp"
+
 
 function App() {
+
+
 
   return (
     <>
@@ -13,13 +21,18 @@ function App() {
           <div className="title-mark">
             <div className="title-notif-count">
               <h1>Notifications</h1>
-              <label>3</label>
+              <label>7</label>
             </div>
             <a>Mark all as read</a>
           </div>
           <Notification imgSource = {markWebberProfile} personName="Mark Webber" message='reacted to your recent post' mainContent='My first tornament today!' time='1m ago'/>
           <Notification imgSource = {angelaGrey} personName="Angela Grey" message='followed you' time='5m ago'/>
-          <Notification imgSource = {jacobThompson} personName="Jacob Thompson" message='has joined your group' mainContent='Chess Club' time='1 day ago'/>
+          <Notification imgSource = {jacobThompson} personName="Jacob Thompson" message='has joined your group' group='Chess Club' time='1 day ago'/>
+          <Notification imgSource = {rizkyHasanuddin} personName="Rizky Hasanuddin" message='sent you a private message' time='5 days ago' 
+          privateMessage = "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I am having lots of fun and improving the game"/>
+          <Notification imgSource = {kimberlySmith} personName="Kimberly Smith" message='commented on your picture' time='1 week ago' postPhotoSource={imageChess}/>
+          <Notification imgSource = {nathanPeterson} personName="Nathan Peterson" message='reacted to your recent post' mainContent='5 end-game strategies to increase win rate' time='2 weeks ago'/>
+          <Notification imgSource = {annaKim} personName="Anna Kim" message='left the group' group='Chess Club' time='2 weeks ago'/>
         </div>
       </main>
     </>
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 
-function Notification({imgSource = "", personName = "", message = "", mainContent = "", time = ""}) {
+function Notification({imgSource = "", personName = "", message = "", mainContent = "", group = "", time = "", postPhotoSource = "", privateMessage = ""}) {
 
     return(
-        <div id="individualNnotif">
-            <img src={imgSource} alt="profilePhoto" />
-            <div className="content-time">
-                <div className="content">
-                    <span>{personName} </span>
-                    <span>{message} </span>
-                    <span>{mainContent} </span>
+        <div className="allNotifs">
+            <div id="individualNnotif">
+                <img src={imgSource} alt="profilePhoto" />
+                <div className="content-time">
+                    <div className="content">
+                        <span>{personName} </span>
+                        <span>{message} </span>
+                        {mainContent != "" && <span className="mainContent">{mainContent} </span>}
+                        {group != "" && <span className="group">{group} </span>}
+                    </div>
+                    <p>{time}</p>
                 </div>
-                <p>{time}</p>
+                {postPhotoSource != "" && <img src={postPhotoSource} alt="profilePhoto" />}
             </div>
-
+            <div>
+                {privateMessage != "" && <div className="private-message">
+                    {privateMessage}
+                </div> }
+            </div>
         </div>
     );
 }
